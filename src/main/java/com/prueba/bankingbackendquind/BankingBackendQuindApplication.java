c98d8c17f37da4890608c253b034a93c670ac2dd
santialar <santiagoalarconvargas1@gmail.com>
2025-09-17 09:44:41 -0500

    se inicializa proyecto

---
+++ src/main/java/com/prueba/bankingbackendquind/BankingBackendQuindApplication.java
@@ -0,0 +1,13 @@
+package com.prueba.bankingbackendquind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingBackendQuindApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankingBackendQuindApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/prueba/bankingbackendquind/BankingBackendQuindApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prueba.bankingbackendquind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingBackendQuindApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
